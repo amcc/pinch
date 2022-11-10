0da24c6934816451cfb37682e3b982fbb8649b0e
amcc <alistairmcclymont@gmail.com>
2022-11-10 14:13:50 +0000

    feat: positioning updates

--- sketch.js
+++ sketch.js
@@ -4,13 +4,64 @@ let xoff, yoff;
 let hand = [];
 let string = "Pinch!";
 let vidWidth, vidHeight;
-let ratio = 480 / 640;
+let myCanvas;
+
+let constraints = {
+  video: {
+    // mandatory: {
+    //   minWidth: 1280,
+    //   minHeight: 720
+    // },
+    video: {
+      width: { ideal: 640 },
+      height: { ideal: 640 },
+    },
+    optional: [{ maxFrameRate: 20 }],
+  },
+  audio: false,
+};
+
+function getCameraStream() {
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((mediaStream) => {
+      const video = document.querySelector("video");
+      video.srcObject = mediaStream;
+      video.onloadedmetadata = () => {
+        video.play();
+        // console.log(video)
+        vidWidth = video.width;
+        vidHeight = video.height;
+        myCanvas = createCanvas(
+          windowWidth,
+          (windowWidth * vidHeight) / vidWidth
+        );
+        myCanvas.parent("canvas-parent");
+        let containerEl = document.getElementById("video-parent");
+        // let videoEl = document.getElementsByTagName('video');
+        // console.log(videoEl)
+        containerEl.appendChild(video);
+      };
+    })
+    .catch((err) => {
+      // always check for errors at the end.
+      console.error(`${err.name}: ${err.message}`);
+    });
+}
+
+function preload() {
+  getCameraStream();
+}
 
 function setup() {
-  createCanvas(windowWidth, windowWidth * ratio);
-  video = createCapture(VIDEO);
+  //
+  //   if(vidWidth && vidHeight) {createCanvas(windowWidth, windowWidth * vidHeight/vidWidth)}
+
+  video = createCapture(constraints, function (stream) {});
+
+  // video = createCapture(VIDEO);
   // console.log(video)
-  video.size(width, height);
+  // video.size(width, height);
   const options = {
     flipHorizontal: true, // boolean value for if the video should be flipped, defaults to false
     maxContinuousChecks: Infinity, // How many frames to go without running the bounding box detector. Defaults to infinity, but try a lower value if the detector is consistently producing bad predictions.
@@ -27,18 +78,17 @@ function setup() {
   });
 
   // Hide the video element, and just show the canvas
-  video.hide();
+  // video.hide();
   textFont("Recursive");
 }
 
 function modelReady() {
-  select("#status").html(
-    "Hold your hand up to the camera"
-  );
+  select("#status").html("Hold your hand up to the camera");
 }
 
 function draw() {
   clear();
+  background(0);
   // image(video, 0, 0, width, height);
 
   // We can call both functions to draw all keypoints and the skeletons
@@ -62,25 +112,30 @@ function makeText() {
       dig2 = index;
     }
 
+    let d1x = (dig1[3][0] * width) / vidWidth;
+    let d1y = (dig1[3][1] * height) / vidHeight;
+    let d2x = (dig2[3][0] * width) / vidWidth;
+    let d2y = (dig2[3][1] * height) / vidHeight;
+
     push();
     stroke(0, 0, 255);
     strokeWeight(1);
-    line(dig1[3][0], dig1[3][1], dig2[3][0], dig2[3][1]);
+    line(d1x, d1y, d2x, d2y);
     stroke(255, 0, 0);
 
-    let d = dist(dig1[3][0], dig1[3][1], dig2[3][0], dig2[3][1]);
+    let d = dist(d1x, d1y, d2x, d2y);
 
     let textScaler = (d / string.length) * 2;
 
     push();
     noStroke();
-    translate(dig1[3][0], dig1[3][1]);
-    let a = atan2(dig2[3][1] - dig1[3][1], dig2[3][0] - dig1[3][0]);
+    translate(d1x, d1y);
+    let a = atan2(d2y - d1y, d2x - d1x);
     rotate(a);
     textSize(textScaler);
 
     noStroke();
-    fill(0);
+    fill(255);
     text(string, 0, 0);
     pop();
   }
@@ -95,14 +150,18 @@ function drawKeypoints() {
 
       push();
       noStroke();
-      fill(0, 100);
-      circle(keypoint[0], keypoint[1], 10);
+      fill(255, 100);
+      circle(
+        (keypoint[0] * width) / vidWidth,
+        (keypoint[1] * height) / vidHeight,
+        10
+      );
       pop();
     }
   }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowWidth * ratio);
-  video.size(width, height);
+  resizeCanvas(windowWidth, (windowWidth * vidHeight) / vidWidth);
+  // video.size(width, height);
 }
